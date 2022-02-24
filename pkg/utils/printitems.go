33bd655cec9e248fc36d5a7ceef5d3e7e29a9fe3
berke <kaancetinkayasf@gmail.com>
2022-02-24 21:30:52 +0300

    displaying the file's percentage in total size

--- pkg/utils/printitems.go
+++ pkg/utils/printitems.go
@@ -7,18 +7,25 @@ import (
 	"github.com/dustin/go-humanize"
 )
 
+var totalSize float32
+
 func PrintItemsByValue(dictionary map[string]int) {
 
 	keys := make([]string, 0, len(dictionary))
 	for key := range dictionary {
 		keys = append(keys, key)
+		totalSize += float32(dictionary[key])
 	}
 
+	fmt.Printf("Total Size: %fKB\n", totalSize)
 	sort.Slice(keys, func(i, j int) bool { return dictionary[keys[i]] > dictionary[keys[j]] })
 
 	for _, key := range keys {
-		fmt.Printf("Filename: %s\n", key)
-		fmt.Printf("Size: %s KB\n", humanize.Commaf(float64(dictionary[key])))
+
 		fmt.Println("------------------------------------------------")
+
+		fmt.Printf("Filename: %s\n", key)
+		fmt.Printf("Size: %s KB | %% %.f of total space\n", humanize.Commaf(float64(dictionary[key])), float32(dictionary[key])/totalSize*100)
+
 	}
 }
